70b80c19dcb29cf9ddc441795e153a03da725671
Pasoon Azimi <pasoon.azimi10@gmail.com>
2018-04-05 15:30:39 -0400

    more changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,16 +3,30 @@ import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom'
 import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap'
 //import "./Home.css"
-//require ('../sass/main.scss');
+require ('../sass/main.scss');
 
 export default class Home extends Component {
     render(){
         return(
             <Grid>
-                <Jumbotron>
+                <Jumbotron id = "main-jumbotron">
                     <h2>Munchies</h2>
                     <p>What are you craving?</p>
                 </Jumbotron>
+                <Row className="show-grid text-center">
+                    <Col xs={12} sm={4} className="resto-wrapper">
+                        <h3>Resto #1</h3>
+                        <p>Whats gooooood in the hooood</p>
+                    </Col>
+                    <Col xs={12} sm={4} className="resto-wrapper">
+                        <h3>Resto #2</h3>
+                        <p>Whats gooooood in the hooood</p>
+                    </Col>
+                    <Col xs={12} sm={4} className="resto-wrapper">
+                        <h3>Resto #2</h3>
+                        <p>Whats gooooood in the hooood</p>
+                    </Col>
+                </Row>
             </Grid>
         )
     }
