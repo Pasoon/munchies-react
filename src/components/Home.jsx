61f26ec4f59ee02d431ee922fa140871b9b02458
Pasoon Azimi <pasoon.azimi10@gmail.com>
2018-04-05 17:36:55 -0400

    updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,63 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-import { Jumbotron, Grid, Row, Col, Image, Button, Form } from 'react-bootstrap'
 import Featured from './featured.jsx';
+import { Jumbotron, Grid, Row, Col, Image, Button, FormControl, ControlLabel, HelpBlock, InputGroup, FormGroup } from 'react-bootstrap'
+
+class FormExample extends Component {
+    constructor(props, context) {
+      super(props, context);
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+
+      this.state = {
+        value: '',
+        data: [],
+      };
+    }
+
+    getValidationState() {
+      const length = this.state.value.length;
+      if (length > 0) return 'success';
+      return null;
+    }
+
+    handleChange(e) {
+      this.setState({ value: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+        console.log("YOOOOOO")
+        alert('Endpoint: http://127.0.0.1:5000/restaurantByName/'+this.state.value);
+        fetch('http://127.0.0.1:5000/restaurantByName/'+this.state.value)
+        .then((response) => response.json())
+        .then((findresponse) => {
+        this.setState({
+        data:findresponse.items,
+        })
+        })
+
+      }
+
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup
+            controlId="formBasicText"
+            validationState={this.getValidationState()}
+          >
+            <FormControl
+              type="text"
+              value={this.state.value}
+              placeholder="Restaurant Name..."
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+        </form>
+      );
+    }
+  }
 
 export default class Home extends Component {
     render(){
@@ -10,6 +66,7 @@ export default class Home extends Component {
                 <section id = "main-jumbotron">
                     <h1>Munchies</h1>
                     <p>What are you craving?</p>
+                    <FormExample id = "search-bar" />
                 </section>
                 <Featured />
             </Grid>
