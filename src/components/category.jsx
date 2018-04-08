05bae50f10fe566a3bed2a9e55ddbfe1c2473851
Zarif Shahriar <zarif.shahriar@gmail.com>
2018-04-07 22:01:06 -0400

    Added categories section

--- src/components/category.jsx
+++ src/components/category.jsx
@@ -1,11 +1,60 @@
 import React, {Component} from 'react';
-import RestaurantCard from './restaurant-card.jsx';
+import CategoryCard from './category-card.jsx';
 
 export default class Category extends Component {
-    render () {
+    constructor() {
+        super();
+        this.state = {
+            categories: [
+                "American",
+                "Asian",
+                "Indian",
+                "Italian",
+                "Mexican",
+                "Seafood",
+                "French",
+                "Canadian"
+            ],
+            categoryUrls: [
+                "http://aui.adelphi.edu/wp-content/uploads/2017/01/American_Foods_ADE.jpg", 
+                "https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2017/10/chinese-beef-aubergine-hotpot.jpg?itok=7obVX214", 
+                "http://www.indianmasalabistro.com/images/banner2.jpg",
+                "https://i.ndtvimg.com/i/2015-12/italian_625x350_41450863014.jpg",
+                "https://www.visitstockton.org/images/made/images/remote/https_files.idssasp.com/public/C102/0740e0c3-1adc-45f6-902c-e09b99d7a4ab/4cc8d671-c1b3-4114-8448-9274ffcb5379_768_432auto_s_c1.jpg",
+                "https://www.makemytrip.com/travel-guide/media/dg_image/maldives/Sea-food.jpg",
+                "https://lefrancophile.com/wp-content/uploads/2010/02/french-baguette.gif",
+                "http://www.readersdigest.ca/wp-content/uploads/2016/01/poutine-canadian-dishes.jpg"
+            ],
+            categoryComponents: []
+        }
+    }
+    componentWillMount() {
+        let items = this
+            .state
+            .categories
+            .map((item, i) => {
+                let category = {
+                    name: item,
+                    picture: this.state.categoryUrls[i]
+                }
+                return (
+                    <div key={item}>
+                        <CategoryCard category={category}/>
+                    </div>
+                )
+            });
+        this.setState({categoryComponents: items});
+    }
+    render() {
         return (
             <section className="categorySection">
-                <h1>Browse By Category</h1>
+                <div className="categoryHeader">
+                    <h1>Categories</h1>
+                    <p>Everyone loves these.</p>
+                </div>
+                <div className="categoryRow">
+                    {this.state.categoryComponents}
+                </div>
             </section>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import App from './app.jsx';
 require('./sass/main.scss');
 
-ReactDOM.render( 
-    <App /> ,
+ReactDOM.render( <
+    App / > ,
     document.getElementById("root")
 );
