76d75da6fe5386ca1a9f1ce26b1d50dd4463fdc8
Pasoon Azimi <pasoon.azimi10@gmail.com>
2018-04-08 20:09:58 -0400

    can now post a new restaurant to database

--- src/components/addrestaurantmodal.jsx
+++ src/components/addrestaurantmodal.jsx
@@ -17,7 +17,7 @@ import {
     Overlay,
     OverlayTrigger,
     FieldGroup,
-    Form
+    Form,
 } from 'react-bootstrap';
 
 export default class AddRestaurantModal extends Component {
@@ -26,9 +26,18 @@ export default class AddRestaurantModal extends Component {
 
         this.handleShow = this.handleShow.bind(this);
         this.handleClose = this.handleClose.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleTypeChange = this.handleTypeChange.bind(this);
+        this.handleUrlChange = this.handleUrlChange.bind(this);
+        this.handleImgUrlChange = this.handleImgUrlChange.bind(this);
+        this.handleAdd = this.handleAdd.bind(this);
 
         this.state = {
-          show: false
+          show: false,
+          name: '',
+          type: '',
+          url: '',
+          img_url: ''
         };
       }
 
@@ -40,8 +49,39 @@ export default class AddRestaurantModal extends Component {
         this.setState({ show: true });
       }
 
-      handleAdd(){
-          
+      handleNameChange(e){
+        this.setState({name: e.target.value});
+     }
+
+     handleTypeChange(e){
+        this.setState({type: e.target.value});
+     }
+
+     handleUrlChange(e){
+        this.setState({url: e.target.value});
+     }
+
+     handleImgUrlChange(e){
+        this.setState({img_url: e.target.value});
+     }
+
+      handleAdd(e){
+          var new_restaurant = {
+              name: this.state.name,
+              type: this.state.type,
+              url: this.state.url,
+              pic_url: this.state.img_url
+          }
+          console.log(new_restaurant);
+          fetch('http://127.0.0.1:5000/post_resto', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify(new_restaurant)
+        }).then(function(res){ console.log(res) })
+        .catch(function(res){ console.log(res) })
       }
 
     render(){
@@ -55,47 +95,47 @@ export default class AddRestaurantModal extends Component {
                     </Modal.Header>
                     <Modal.Body>
                         <Form horizontal>
-                        <FormGroup controlId="formHorizontalRName">
-                            <Col componentClass={ControlLabel} sm={3}>
-                            Name:
-                            </Col>
-                            <Col sm={9}>
-                            <FormControl type="text" placeholder="Shawarma Palace" />
-                            </Col>
-                        </FormGroup>
+                            <FormGroup controlId="formHorizontalRName">
+                                <Col componentClass={ControlLabel} sm={3}>
+                                Name:
+                                </Col>
+                                <Col sm={9}>
+                                <FormControl type="text" placeholder="Shawarma Palace" value={this.state.name} onChange={this.handleNameChange} />
+                                </Col>
+                            </FormGroup>
 
-                        <FormGroup controlId="formHorizontalType">
-                            <Col componentClass={ControlLabel} sm={3}>
-                            Type:
-                            </Col>
-                            <Col sm={9}>
-                            <FormControl type="text" placeholder="Lebanese" />
-                            </Col>
-                        </FormGroup>
+                            <FormGroup controlId="formHorizontalType">
+                                <Col componentClass={ControlLabel} sm={3}>
+                                Type:
+                                </Col>
+                                <Col sm={9}>
+                                <FormControl type="text" placeholder="Lebanese" value={this.state.type} onChange={this.handleTypeChange}  />
+                                </Col>
+                            </FormGroup>
 
-                        <FormGroup controlId="formHorizontalWebUrl">
-                            <Col componentClass={ControlLabel} sm={3}>
-                            Website URL:
-                            </Col>
-                            <Col sm={9}>
-                            <FormControl type="website" placeholder="http://shawarmapalace.ca/" />
-                            </Col>
-                        </FormGroup>
+                            <FormGroup controlId="formHorizontalWebUrl">
+                                <Col componentClass={ControlLabel} sm={3}>
+                                Website URL:
+                                </Col>
+                                <Col sm={9}>
+                                <FormControl type="website" placeholder="http://shawarmapalace.ca/" value={this.state.url} onChange={this.handleUrlChange} />
+                                </Col>
+                            </FormGroup>
 
-                        <FormGroup controlId="formHorizontalRImgUrl">
-                            <Col componentClass={ControlLabel} sm={3}>
-                            Image URL:
-                            </Col>
-                            <Col sm={9}>
-                            <FormControl type="text" placeholder="http://shawarmapalace.ca/wp-content/uploads/2017/06/25-1920x1000.jpg" />
-                            </Col>
-                        </FormGroup>
+                            <FormGroup controlId="formHorizontalImgUrl">
+                                <Col componentClass={ControlLabel} sm={3}>
+                                Image URL:
+                                </Col>
+                                <Col sm={9}>
+                                <FormControl type="text" placeholder="http://shawarmapalace.ca/wp-content/uploads/2017/06/25-1920x1000.jpg" value={this.state.img_url} onChange={this.handleImgUrlChange}/>
+                                </Col>
+                            </FormGroup>
 
-                        <FormGroup>
-                            <Col smOffset={3} sm={10}>
-                            <Button type="submit" onClick={this.handleAdd}>Add</Button>
-                            </Col>
-                        </FormGroup>
+                            <FormGroup>
+                                <Col smOffset={3} sm={10}>
+                                <Button type="submit" onClick={this.handleAdd}>Add</Button>
+                                </Col>
+                            </FormGroup>
                         </Form>
                     </Modal.Body>
                     <Modal.Footer>
