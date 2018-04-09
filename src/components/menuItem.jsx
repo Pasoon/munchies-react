1a4e7522cc5156c4e8c7f8b2d3f4ae275e27a64c
Zarif Shahriar <zarif.shahriar@gmail.com>
2018-04-08 20:35:27 -0400

    Updated to menu

--- src/components/menuItem.jsx
+++ src/components/menuItem.jsx
@@ -1,6 +1,33 @@
 import React, {Component} from 'react';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 export default class MenuItem extends Component {
+    constructor() {
+        super();
+        this.onAfterInsertRow = this
+            .onAfterInsertRow
+            .bind(this)
+        this.onAfterDeleteRow = this
+            .onAfterDeleteRow
+            .bind(this)
+
+        this.state = {
+            options: {
+                afterInsertRow: this.onAfterInsertRow,
+                afterDeleteRow: this.onAfterDeleteRow // A hook for after insert rows
+            }
+        }
+
+    }
+    onAfterInsertRow(row) {
+        let newRowStr = '';
+        for (const prop in row) {
+            newRowStr += prop + ': ' + row[prop] + ' \n';
+        }
+        alert('The new row is:\n ' + newRowStr);
+    }
+    onAfterDeleteRow(rowKeys) {
+        alert('The rowkey you drop: ' + rowKeys);
+    }
     render() {
         console.log(this.props.items)
         if (!this.props.items) {
@@ -12,8 +39,18 @@ export default class MenuItem extends Component {
         }
         return (
             <div className="menuTable">
-                <BootstrapTable data={this.props.items} striped hover bordered={ false } condensed
-                height='400' scrollTop={ 'Top' } className='menuTableColumns'>
+                <BootstrapTable
+                    data={this.props.items}
+                    striped
+                    hover
+                    bordered={false}
+                    condensed
+                    height='400'
+                    scrollTop={'Top'}
+                    className='menuTableColumns'
+                    insertRow={true}
+                    options={this.state.options}
+                    deleteRow={true}>
                     <TableHeaderColumn isKey dataField='name'>Name</TableHeaderColumn>
                     <TableHeaderColumn dataField='category'>Category</TableHeaderColumn>
                     <TableHeaderColumn dataField='type'>Type</TableHeaderColumn>
