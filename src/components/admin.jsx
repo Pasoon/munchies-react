4efd0ee4b463bf786c5dc25c289383ca06dc536b
Zarif Shahriar <zarif.shahriar@gmail.com>
2018-04-08 22:39:26 -0400

    Updated admin

--- src/components/admin.jsx
+++ src/components/admin.jsx
@@ -10,23 +10,24 @@ import {
     InputGroup,
     FormGroup,
     FieldGroup,
-    Form,
+    Form
 } from 'react-bootstrap';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
-export default class Admin extends Component{
-    constructor(props){
+export default class Admin extends Component {
+    constructor(props) {
         super(props);
 
-        this.handleSelectionChange = this.handleSelectionChange.bind(this);
+        this.handleSelectionChange = this
+            .handleSelectionChange
+            .bind(this);
 
         var dummydata = [
             {
                 name: 'Zeeshah',
                 type: 'bangla',
                 id: '3'
-            },
-            {
+            }, {
                 name: 'Pasoon',
                 type: 'afghan',
                 id: '2'
@@ -34,15 +35,15 @@ export default class Admin extends Component{
         ]
         this.state = {
             value: 'select',
-            dummydata: dummydata
+            dummydata: dummydata,
+            columns: Object.keys(dummydata[0])
         }
+
     }
 
     handleSelectionChange(e) {
-        this.setState({ value: e.target.value });
-
-    
-      }
+        this.setState({value: e.target.value});
+    }
 
     render() {
         return (
@@ -52,28 +53,26 @@ export default class Admin extends Component{
                     <p>Query our databases directly.</p>
                 </div>
                 <div className="adminQuerySection">
-                <FormGroup controlId="formControlsSelect">
-                    <ControlLabel>Select a Query</ControlLabel>
-                    <FormControl componentClass="select" onChange={this.handleSelectionChange} value={this.state.value} placeholder="select">
-                        <option value="select">Query 1</option>
-                        <option value="select">Query 2</option>
-                        <option value="select">Query 3</option>
-                        <option value="select">Query 4</option>
-                        <option value="select">Query 5</option>
-                        <option value="other">...</option>
-                    </FormControl>
-                </FormGroup>
-                <Button bsStyle="primary" bsSize="large">Retrieve Data</Button>
+                    <FormGroup controlId="formControlsSelect">
+                        <ControlLabel>Select a Query</ControlLabel>
+                        <FormControl
+                            componentClass="select"
+                            onChange={this.handleSelectionChange}
+                            value={this.state.value}
+                            placeholder="select">
+                            <option value="select">Query 1</option>
+                            <option value="select">Query 2</option>
+                            <option value="select">Query 3</option>
+                            <option value="select">Query 4</option>
+                            <option value="select">Query 5</option>
+                            <option value="other">...</option>
+                        </FormControl>
+                    </FormGroup>
+                    <Button bsStyle="primary" bsSize="large">Retrieve Data</Button>
                 </div>
                 <div className="adminRetrievedDataSection">
-                    <BootstrapTable data ={this.state.dummydata} striped hover bordered={ false } condensed
-                    height='400' scrollTop={ 'Top' } className='dataTableColumns'>
-                    {
-                        this.state.dummydata.map((row, index)=>{
-                                console.log("YESS SIRRRRR",row)
-                                return <TableHeaderColumn dataField={row[index]}>{`${column}`}</TableHeaderColumn>
-                                })
-                    }
+                    <BootstrapTable data={this.state.dummydata} keyField='id'>
+                        {this.state.columns.map(column => <TableHeaderColumn dataField={column}>{column}</TableHeaderColumn>)}
                     </BootstrapTable>
                 </div>
             </section>
